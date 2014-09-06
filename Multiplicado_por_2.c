2db16dfa664dc8c626951d8a8b1aef2d57e1fdef
Maximo Rodriguez <maximo1225@gmail.com>
2014-09-06 11:56:26 -0400

    Readme.txt

---
+++ Multiplicado_por_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  int entero, resultado;
+  printf("introduzca un valor:\n");
+  scanf("%d",&entero);
+  printf("entero: %d\n",entero);
+  resultado = entero*2;
+  printf("Multiplicacion por 2: %d",resultado);
+  return 0;
+}
--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,1 @@
-This file is a test.
+Aquí estarán problemas de programación algorítmica mientras aprendo la lógica y programación del lenguaje c
