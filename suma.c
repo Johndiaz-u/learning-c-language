80b936dd7718564a69b5f09d8b0e136881463b46
Maximo Rodriguez <maximo1225@gmail.com>
2014-09-07 15:28:32 -0400

    suma.c

---
+++ suma.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int a, b, suma;
+	printf("Introduzca los dos valores de la suma: ");
+	scanf("%d %d",&a, &b);
+	suma = a+b;
+	printf("La suma de A+B = %d\n", suma);
+	return 0;
+}
